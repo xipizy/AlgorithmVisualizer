347ea1f2f73ce14f74b6c086e21006d669ebcc73
Eloon Ing <eing067@aucklanduni.ac.nz>
2025-07-15 12:24:00 +1200

    Set up basic componets of the stage and the title for the entry scene

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,12 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -12,7 +18,25 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Stage stage = new Stage();
+        Group root = new Group();
+        Scene scene = new Scene(root, 1000, 1000, Color.GREY);
+        Image icon = new Image("icon.png");
 
+
+        Text title = new Text();
+        title.setText("Algorithm Visualizer");
+        title.setFont(Font.font("Verdana",50));
+        title.setFill(Color.WHITE);
+        title.setY(100);
+        title.setX(250);
+        root.getChildren().add(title);
+
+        // Setting Parameters of Stage
+        stage.setResizable(false);
+
+        stage.getIcons().add(icon);
+        stage.setTitle("Algorithm Visualizer");
+        stage.setScene(scene);
         stage.show();
     }
 }
